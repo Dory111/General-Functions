da0effdaeb4bd13fa232fe5c4c8896cd20e31595
Dory111 <57412532+Dory111@users.noreply.github.com>
2025-07-09 19:26:25 -0700

    Changed percentage ticks to exponentially scale to max value
    Better estimates the percentage marks as a weighted average of the base data, where the weighted average weights are scaled based on user options

--- Decide_Percentage_Marks.R
+++ Decide_Percentage_Marks.R
@@ -1,3 +1,22 @@
+################################################################################
+############################## EXAMPLE #########################################
+################################################################################
+# base_values <- runif(n = 50000, min = 0, max = 100) %>% round(0)
+# new_values <- runif(n = 49999, min = 0, max = 100) %>% round(0)
+# new_values <- append(new_values, 110)
+# plot(new_values-base_values,
+#      type = 'l')
+# axis(4,
+#      at = axTicks(2),
+#      labels = Decide_Percentage_Marks(base_values = base_values,
+#                                       new_values = new_values,
+#                                       plot_axis = axTicks(2),
+#                                       round = 1,
+#                                       invert = FALSE,
+#                                       weight_power = 'exp',
+#                                       weight_divisor = 100,
+#                                       scale = 'difference'))
+
 #===============================================================================
 # Estimates an axis sequence for the percentage change between base values
 # and new values
@@ -5,8 +24,10 @@
 Decide_Percentage_Marks <- function(base_values,
                                     new_values,
                                     plot_axis = axTicks(2),
-                                    round = 3,
+                                    round = 1,
                                     invert = FALSE,
+                                    weight_power = 'exp',
+                                    weight_divisor = 100,
                                     scale = 'difference')
 {
   # -------------------------------------------------------------------------------------
@@ -15,20 +36,49 @@ Decide_Percentage_Marks <- function(base_values,
     # -------------------------------------------------------------------------------------
     # Plotting difference or real values?
     if(invert == TRUE){
-      avoid_nans <- ((base_values - new_values)/new_values)*100
+      # -------------------------------------------------------------------------------------
+      # how to set the weights depending on the value range
+      if(weight_power == 'exp'){
+        weights <- new_values/(max(new_values)/weight_divisor)
+        weights[new_values == 0] <- 0
+        weights <- exp(weights)
+      } else {
+        weights <- (base_values**weight_power)
+        weights[base_values == 0] <- 0
+        avoid_nans <- (plot_axis/(weighted.mean(x = base_values,
+                                                w = weights)))*100
+      }
+      # -------------------------------------------------------------------------------------
     } else {
-      avoid_nans <- ((new_values - base_values)/base_values)*100
+      # -------------------------------------------------------------------------------------
+      # how to set the weights depending on the value range
+      if(weight_power == 'exp'){
+        weights <- base_values/(max(base_values)/weight_divisor)
+        weights[base_values == 0] <- 0
+        weights <- exp(weights)
+      } else {
+        weights <- (new_values**weight_power)
+        weights[new_values == 0] <- 0
+        avoid_nans <- (plot_axis/(weighted.mean(x = new_values,
+                                                w = weights)))*100
+      }
+      # -------------------------------------------------------------------------------------
+      avoid_nans <- (plot_axis/(weighted.mean(x = base_values,
+                                              w = weights)))*100
     }
     # -------------------------------------------------------------------------------------
-    
+
     # -------------------------------------------------------------------------------------
     # Get ticks
     range <- range(avoid_nans[is.nan(avoid_nans) == FALSE &
-                                is.na(avoid_nans) == FALSE &
-                                is.infinite(avoid_nans) == FALSE])
+                              is.na(avoid_nans) == FALSE &
+                              is.infinite(avoid_nans) == FALSE])
     percentages_sequence <- seq(from = range[1],
                                 to = range[2],
                                 length.out = length(plot_axis))
+    if(0 %in% plot_axis){
+      percentages_sequence[which(plot_axis == 0)] <- 0
+    }
     percentages_sequence <- round(percentages_sequence,round)
     # -------------------------------------------------------------------------------------
     
@@ -69,6 +119,9 @@ Decide_Percentage_Marks <- function(base_values,
                                   length.out = length(plot_axis))
     }
     # -------------------------------------------------------------------------------------
+    if(0 %in% plot_axis){
+      percentages_sequence[which(plot_axis == 0)] <- 0
+    }
     percentages_sequence <- round(percentages_sequence,round)
   }
   # -------------------------------------------------------------------------------------
