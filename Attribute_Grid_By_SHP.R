e478b234807f82ff35da7b74b709a5e9bd229c4f
Dory111 <57412532+Dory111@users.noreply.github.com>
2025-07-11 16:11:21 -0700

    Case with Null Grid and Long Export

--- Attribute_Grid_By_SHP.R
+++ Attribute_Grid_By_SHP.R
@@ -1,16 +1,24 @@
-Attribute_Grid_By_SHP <- function(wd,
-                                  out_dir,
+###########################################################################################
+############################## MAIN FUNCTION ##############################################
+###########################################################################################
+#===============================================================================
+# Placeholder
+#===============================================================================
+Attribute_Grid_By_SHP <- function(out_dir,
                                   out_name,
                                   shapefile,
                                   model_grid,
+                                  hru_id_column,
                                   replacement_value = NULL,
                                   starting_values = NULL,
+                                  export_as_grid = FALSE,
+                                  export_long_format = TRUE,
                                   is_raster = TRUE,
-                                  grid_dims = NULL,
+                                  grid_dims = c(NULL,NULL),
                                   null_value = -9999,
                                   engulf = TRUE,
                                   partial_fact = NULL,
-                                  scale_by_intersected_area = TRUE)
+                                  scale_by_intersected_area = FALSE)
 {
   # -------------------------------------------------------------------------------------
   # Error handling
@@ -28,6 +36,11 @@ Attribute_Grid_By_SHP <- function(wd,
     stop(paste0('\nAttribute_Grid_By_SHP: REPLACEMENT VALUE NOT SPECIFIED'))
     
   }
+  
+  if(crs(shapefile) != crs(model_grid)){
+    stop(paste0('\nAttribute_Grid_By_SHP: CRS OF SHAPEFILE != CRS OF MODEL GRID'))
+    
+  }
   # -------------------------------------------------------------------------------------
   
   # -------------------------------------------------------------------------------------
@@ -36,6 +49,161 @@ Attribute_Grid_By_SHP <- function(wd,
     warning(paste0('\nAttribute_Grid_By_SHP: No starting values specified, will return grid',
                    '\nof \"-9999\" values where shapefile not intersected'))
   }
+  
+  if(length(grid_dims) == 1){
+    warning(paste0('\nAttribute_Grid_By_SHP: Only one grid dimension specified',
+                   '\ngrid_dims[1] will be used for both dimensions'))
+    
+  }
   # -------------------------------------------------------------------------------------
+  
+  # -------------------------------------------------------------------------------------
+  if(is_raster == FALSE & is.null(starting_values) == TRUE){
+    
+    Start_From_Null_Grid(out_dir,
+                         out_name,
+                         shapefile,
+                         model_grid,
+                         hru_id_column,
+                         replacement_value,
+                         starting_values,
+                         export_as_grid,
+                         export_long_format,
+                         is_raster,
+                         grid_dims,
+                         null_value,
+                         engulf,
+                         partial_fact,
+                         scale_by_intersected_area)
+  }
+  # -------------------------------------------------------------------------------------
+}
+# -------------------------------------------------------------------------------------
+
+
+
+
+
+###########################################################################################
+############################## HELPER FUNCTIONS ###########################################
+###########################################################################################
+#===============================================================================
+# Placeholder
+#===============================================================================
+Start_From_Null_Grid <- function(out_dir,
+                                 out_name,
+                                 shapefile,
+                                 model_grid,
+                                 hru_id_column,
+                                 replacement_value,
+                                 starting_values,
+                                 export_as_grid,
+                                 export_long_format,
+                                 is_raster,
+                                 grid_dims,
+                                 null_value,
+                                 engulf,
+                                 partial_fact,
+                                 scale_by_intersected_area)
+{
+  # -------------------------------------------------------------------------------------
+  # Starting values and area to reach 100% engulf
+  original_colnames <- colnames(model_grid)
+  original_colnames <- append(original_colnames, 'UPDATED_VALUE')
+  model_grid$DUMMY_ID <- c(1:nrow(model_grid))
+  model_grid$UPDATED_VALUE <- rep(-9999,nrow(model_grid))
+  cell_area <- st_area(model_grid[1,])
+  starting_values <- Reconstruct_Null_Grid(grid_dims,
+                                           null_value,
+                                           model_grid)
+  intersected_model_grid <- st_intersection(model_grid,
+                                            shapefile)
+  intersected_model_grid$Percent_Area <- as.numeric(round((st_area(intersected_model_grid)/cell_area) * 100,1))
+  # -------------------------------------------------------------------------------------
+
+  
+  
+  
+  ################################### DECIDING HOW TO UPDATE VALUES #####################
+  # -------------------------------------------------------------------------------------
+  # if entire cell needs to be engulfed then only set cells engulfed to the replacement value
+  
+  # if entire cell DOES NOT need to be engulfed and scale_by_intersected_area is specified
+  # then only set cells covered above some value
+  # of their area to the replacement value multiplied by how much of the cell is covered
+  
+  # if entire cell DOES NOT need to be engulfed and scale_by_intersected_area is  NOT specified
+  # then only set cells covered above some value
+  # of their area to the replacement value
+  if(engulf == TRUE){
+    
+    intersected_model_grid$UPDATED_VALUE[intersected_model_grid$Percent_Area == 100] <- replacement_value
+    
+  } else if(partial_fact == TRUE & scale_by_intersected_area == TRUE){
+    
+    intersected_model_grid$UPDATED_VALUE[intersected_model_grid$Percent_Area >= partial_fact] <-
+      replacement_value*intersected_model_grid$Percent_Area[intersected_model_grid$Percent_Area >= partial_fact]
+    
+  } else if(partial_fact == TRUE & scale_by_intersected_area == FALSE){
+    
+    intersected_model_grid$UPDATED_VALUE[intersected_model_grid$Percent_Area >= partial_fact] <- replacement_value
+      
+  }
+  # -------------------------------------------------------------------------------------
+  
+  # -------------------------------------------------------------------------------------
+  # set model grid equal to replacement values
+  for(i in 1:nrow(model_grid)){
+    if(model_grid$DUMMY_ID[i] %in% intersected_model_grid$DUMMY_ID){
+      model_grid$UPDATED_VALUE[i] <- intersected_model_grid$UPDATED_VALUE[intersected_model_grid$DUMMY_ID == model_grid$DUMMY_ID[i]]
+    }
+  }
+  model_grid <- model_grid[-c(!which(colnames(model_grid) %in% original_colnames)), ]
+  # -------------------------------------------------------------------------------------
+
+  
+  
+  
+  
+  
+  
+  ################################### EXPORT ############################################
+  # -------------------------------------------------------------------------------------
+  if(export_as_grid == TRUE){
+    
+    
+  } else if(export_long_format == TRUE){
+    
+    export_long <- data.frame(HRU_ID = as.vector(unlist(model_grid[hru_id_column])),
+                              UPDATED_VALUE = as.vector(unlist(model_grid$UPDATED_VALUE)))
+    export_long <- export_long[order(export_long$HRU_ID, decreasing = FALSE)]
+    write.csv(x = export_long,
+              file = file.path(out_dir,paste0(out_name,'.csv')),
+              row.names = FALSE)
+    st_write(obj = model_grid,
+             dsn = file.path(out_dir,paste0(out_name,'.shp')),
+             append = FALSE)
+  }
+  # -------------------------------------------------------------------------------------
+}
+# -------------------------------------------------------------------------------------
+
+
+#===============================================================================
+# Placeholder
+#===============================================================================
+Reconstruct_Null_Grid <- function(grid_dims,
+                                  null_value,
+                                  model_grid){
+  
+  raster <- rast(nrow = grid_dims[1],
+                 ncol = grid_dims[2],
+                 xmin = extent(model_grid)[1],
+                 xmax = extent(model_grid)[2],
+                 ymin = extent(model_grid)[3],
+                 ymax = extent(model_grid)[4],
+                 crs = crs(model_grid),
+                 vals = null_value)
+  return(raster)
 }
 # -------------------------------------------------------------------------------------
