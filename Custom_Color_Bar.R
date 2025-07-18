50e6d4e14b178c94e4f3698b537c447facc5f4c4
Dory111 <57412532+Dory111@users.noreply.github.com>
2025-07-18 15:14:31 -0700

    Improved Variable Names and Comments

--- Custom_Color_Bar.R
+++ Custom_Color_Bar.R
@@ -8,70 +8,93 @@ Custom_Color_Bar <- function(colors,
                              labels_at = NULL,
                              labels_text = NULL)
 {
-  
+  #-------------------------------------------------------------------------------
+  # create matrix of the coordinates for the rectangles that constitute the color bar
+  color_mat <- matrix(nrow = length(colors),
+                      ncol = 6)
+  color_mat <- as.data.frame(color_mat)
+  colnames(color_mat) <- c('xleft','ybottom','xright','ytop','value','color')
+  color_mat$xleft <- rep(xleft,nrow(color_mat))
+  color_mat$xright <- rep(xright,nrow(color_mat))
+  #-------------------------------------------------------------------------------
   
   #-------------------------------------------------------------------------------
-  m <- matrix(nrow = length(colors),
-              ncol = 6)
-  m <- as.data.frame(m)
-  colnames(m) <- c('xleft','ybottom','xright','ytop','value','color')
-  m$xleft <- rep(xleft,nrow(m))
-  m$xright <- rep(xright,nrow(m))
-  
-  
+  # as the bars are horizontal the ybottom and ytop need to be incremented
+  # while the xleft and xright are constant
   y_seq <- seq(from = ybot, to = ytop, length.out = length(colors))
-  m$ybottom <- seq(from = y_seq[1], to = tail(y_seq,2)[1], length.out = length(colors))
-  m$ytop <- seq(from = y_seq[2], to = tail(y_seq,1), length.out = length(colors))
-  new_row <- c(xleft,
-               tail(m$ytop,1),
-               xright,
-               tail(m$ytop,1),
-               NA,
-               NA)
+  color_mat$ybottom <- seq(from = y_seq[1], to = tail(y_seq,2)[1], length.out = length(colors))
+  color_mat$ytop <- seq(from = y_seq[2], to = tail(y_seq,1), length.out = length(colors))
+  #-------------------------------------------------------------------------------
   
-  m$value <- seq(from = -1, to = 1, length.out = length(colors))
-  m$color <- colors
+  #-------------------------------------------------------------------------------
+  color_mat$value <- seq(from = -1, to = 1, length.out = length(colors))
+  color_mat$color <- colors
   y_range <- ytop-ybot
   #-------------------------------------------------------------------------------
-
-
+  
   #-------------------------------------------------------------------------------
-  for(j in 1:nrow(m)){
-    rect(m$xleft[j],
-         m$ybottom[j],
-         m$xright[j],
-         m$ytop[j],
-         col = m$color[j],
-         border = NA)
+  # draw the rectangles of the color bar
+  for(j in 1:nrow(color_mat)){
+    #-------------------------------------------------------------------------------
+    # compensates that the first label in the bar might be drawn below where the
+    # inner portion of the rectangle is by setting the first border of the color bar
+    # to the first color while the other rectangles that make up the bar have their border
+    # color set to NA
+    if(j == 1){
+      rect(color_mat$xleft[j],
+           color_mat$ybottom[j],
+           color_mat$xright[j],
+           color_mat$ytop[j],
+           col = color_mat$color[j],
+           border = NA)
+      lines(x = c(xright, xright + line_length),
+            y = c(color_mat$ybottom[j],color_mat$ybottom[j]))
+    } else {
+      rect(color_mat$xleft[j],
+           color_mat$ybottom[j],
+           color_mat$xright[j],
+           color_mat$ytop[j],
+           col = color_mat$color[j],
+           border = NA)
+    }
+    #-------------------------------------------------------------------------------
   }
   #-------------------------------------------------------------------------------
   
-  m <- rbind(m,new_row)
   #-------------------------------------------------------------------------------
-  if(labels_TF == T){
-    if(is.null(labels_text) == T){
+  # create extra row for the last label to be drawn with
+  # compensates for the fact that in any color bar there will be a different
+  # number of colors and breaks necessarily
+  new_row <- c(xleft,
+               tail(color_mat$ytop,1),
+               xright,
+               tail(color_mat$ytop,1),
+               NA,
+               NA)
+  color_mat <- rbind(color_mat,new_row)
+  #-------------------------------------------------------------------------------
+  
+
+  #-------------------------------------------------------------------------------
+  # should labels be plotted on the color bar
+  if(labels_TF == TRUE){
+    #-------------------------------------------------------------------------------
+    # if user did not enter labels then set them to the break values
+    if(is.null(labels_text) == TRUE){
       labels_text <- labels_at
     }
     #-------------------------------------------------------------------------------
+    
+    #-------------------------------------------------------------------------------
+    # draw labels
     for(j in 1:length(labels_at)){
-      if(j == 1){
         lines(x = c(xright, xright + line_length),
-              y = c(m$ybottom[j] + y_range*0.001,m$ybottom[j]+ y_range*0.001))
+              y = c(color_mat$ybottom[j],color_mat$ybottom[j]))
         text(x = xright + line_length,
-             y = m$ybottom[j], 
+             y = color_mat$ybottom[j], 
              pos = 4,
              labels = labels_text[j],
              cex = 1.4)
-      } else {
-        lines(x = c(xright, xright + line_length),
-              y = c(m$ybottom[j],m$ybottom[j]))
-        text(x = xright + line_length,
-             y = m$ybottom[j], 
-             pos = 4,
-             labels = labels_text[j],
-             cex = 1.4)
-      }
-      #-------------------------------------------------------------------------------
     }
     #-------------------------------------------------------------------------------
   }
